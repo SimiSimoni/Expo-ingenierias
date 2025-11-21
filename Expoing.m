cfd99352916235fc60fc7f64f226bf1972b91761
SimiSimoni <ninasimonefaulkner@gmail.com>
2025-11-21 17:46:30 -0600

    v 1.2
    Cambie el colormap por textura

--- Expoing.m
+++ Expoing.m
@@ -1,5 +1,7 @@
 clc; clear; close all;
 
+textura=imread("C:\Users\Simone\Downloads\textura.jpg");
+
 % volcan
 [x, y] = meshgrid(-5:0.1:5, -5:0.1:5);
 r = sqrt(x.^2 + y.^2);
@@ -7,8 +9,7 @@ r = sqrt(x.^2 + y.^2);
 z = exp(-0.1*r.^2) * 5 - 2*exp(-r.^2);
 z = z + 0.07*randn(size(z));
 
-surf(x*1000, y*1000, z*1000, 'EdgeColor', 'none');
-colormap(spring);
+surf(x*1000, y*1000, z*1000,'CData',textura,'FaceColor','texturemap','EdgeColor','none');
 axis equal
 xlabel('X (m)')
 ylabel('Y (m)')
